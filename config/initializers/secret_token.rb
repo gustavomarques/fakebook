c745c74e9de9ff75fc68631637f365e57f193858
Gustavo Marques <gutooh@gmail.com>
2012-11-05 20:33:15 -0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fakebook::Application.config.secret_token = '3d66de2d7dd8edeaa0a7fb628701a3361414b3525c6d9653429f2ed4dc898d5e4c654f32604086b5bfec75aeeeffa5fede1f69b91b965ff57fedfb066fffc982'
